d32175c2dadaaa06eb213df578b015561fe4daf3
Gikai Andrews <gnaboys@gmail.com>
2024-02-27 16:51:34 -0500

    attempted mobile responsiveness

--- legendofti/src/Navbar/Navbar.js
+++ legendofti/src/Navbar/Navbar.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Navbar.css";
 import { Link, useLocation } from "react-router-dom";
 import tipfp from "../images/ti-pfp.png";
@@ -7,27 +7,54 @@ function Navbar() {
   const location = useLocation();
 
   const isHomePage = location.pathname === "/";
+
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <div className={`navbar ${isHomePage ? "transparent" : "white"}`}>
       <img src={tipfp} alt="ti-pfp" className="tipfp"></img>
-      <ul>
+      <div
+        className={`menu-toggle ${isOpen ? "open" : ""}`}
+        onClick={toggleMenu}
+      >
+        <div className="bar"></div>
+        <div className="bar"></div>
+        <div className="bar"></div>
+      </div>
+      <ul className={`navbar-menu ${isOpen ? "open" : ""}`}>
         <li>
-          <Link to="/" className={`navbar-link ${isHomePage ? "active" : "black"}`}>
+          <Link
+            to="/"
+            className={`navbar-link ${isHomePage ? "active" : "black"}`}
+          >
             Home
           </Link>
         </li>
         <li>
-        <Link to="/about" className={`navbar-link ${isHomePage ? '' : 'black'}`}>
+          <Link
+            to="/about"
+            className={`navbar-link ${isHomePage ? "" : "black"}`}
+          >
             About
           </Link>
         </li>
         <li>
-        <Link to="/gaming" className={`navbar-link ${isHomePage ? '' : 'black'}`}>
+          <Link
+            to="/gaming"
+            className={`navbar-link ${isHomePage ? "" : "black"}`}
+          >
             Gaming
           </Link>
         </li>
         <li>
-        <Link to="/contact" className={`navbar-link ${isHomePage ? '' : 'black'}`}>
+          <Link
+            to="/contact"
+            className={`navbar-link ${isHomePage ? "" : "black"}`}
+          >
             Contact
           </Link>
         </li>
