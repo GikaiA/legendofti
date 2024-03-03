b645c5a99f273195efe06e5c43b5a200523cecca
Gikai Andrews <gnaboys@gmail.com>
2024-03-03 17:42:48 -0500

    started finsihed twitch mobile responsive

--- legendofti/src/Gaming/Gaming.js
+++ legendofti/src/Gaming/Gaming.js
@@ -27,24 +27,10 @@ function Gaming() {
             channel="legendof_ti"
             darkMode={true}
             height={480}
+            className="twitch-chat"
           ></TwitchChat>
         </div>
-        <h1 className="social-title">Twitter/X</h1>
-        <div className="social-container">
-          <TwitterTimelineEmbed
-            sourceType="timeline"
-            screenName="LegendofTi"
-            options={{ height: 800, width: 900 }}
-          ></TwitterTimelineEmbed>
-        </div>
-        <h1 className="social-title">Instagram</h1>
-        <div className="social-container">
-          <InstagramEmbed
-            url="https://www.instagram.com/legendof_ti/"
-            width={900}
-          ></InstagramEmbed>
-        </div>
-        <h1 className="social-title">Youtube</h1>
+        {/* <h1 className="social-title">Youtube</h1>
         <div className="social-container">
           <Carousel
             showArrows={false}
@@ -53,7 +39,8 @@ function Gaming() {
             showStatus={false}
           >
             <div className="thumbnail-container">
-              <Link to="https://youtu.be/X9Sf5pfMekE?si=rswg2lUBzLEquMnH"
+              <Link
+                to="https://youtu.be/X9Sf5pfMekE?si=rswg2lUBzLEquMnH"
                 className="thumbnail-link"
               >
                 <img
@@ -68,7 +55,6 @@ function Gaming() {
               to="https://youtu.be/X9Sf5pfMekE?si=rswg2lUBzLEquMnH"
               className="thumbnail-link"
             >
-
               <img
                 src={tithumbnail2}
                 alt="ti-thumbnail"
@@ -97,6 +83,21 @@ function Gaming() {
             </Link>
           </Carousel>
         </div>
+        <h1 className="social-title">Twitter/X</h1>
+        <div className="social-container">
+          <TwitterTimelineEmbed
+            sourceType="timeline"
+            screenName="LegendofTi"
+            options={{ height: 800, width: 1200 }}
+          ></TwitterTimelineEmbed>
+        </div>
+        <h1 className="social-title">Instagram</h1>
+        <div className="social-container">
+          <InstagramEmbed
+            url="https://www.instagram.com/legendof_ti/"
+            width={900}
+          ></InstagramEmbed>
+        </div> */}
       </div>
     </div>
   );
